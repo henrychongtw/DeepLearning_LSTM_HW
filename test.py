1cbe100d366f21cc6c8be9db0d102fe68dfe1d99
Henry Chong <henrychong@ip209-174.wlan.ntust.edu.tw>
2017-10-30 10:30:12 +0800

    the lstm train with 500000 times copy

--- test.py
+++ test.py
@@ -16,41 +16,17 @@ N_OUTPUT = 1
 
 # from https://github.com/Lasagne/Lasagne/blob/master/examples/recurrent.py
 def gen_data(min_length=MIN_LENGTH, max_length=MAX_LENGTH):
-    '''
-	Generate a sequences for the "add" task, e.g. the target for the
-	following
-	``| 0.5 | 0.7 | 0.3 | 0.1 | 0.2 | ... | 0.5 | 0.9 | ... | 0.8 | 0.2 |
-	  |  0  |  0  |  1  |  0  |  0  |     |  0  |  1  |     |  0  |  0  |``
-	would be 0.3 + .9 = 1.2.  This task was proposed in [1]_ and explored in
-	e.g. [2]_.
-	Parameters
-	----------
-	min_length : int
-		Minimum sequence length.
-	max_length : int
-		Maximum sequence length.
-	Returns
-	-------
-	X : np.ndarray
-		Input to the network, of shape (length, 2), where the last
-		dimension corresponds to the two sequences shown above.
-	y : np.ndarray
-		Correct output for each sample (a scalar).
-	'''
     # Generate x_seq
     length = np.random.randint(min_length, max_length)
     x_seq = np.concatenate([np.random.uniform(size=(length, 1)),
                             np.zeros((length, 1))],
                            axis=-1)
-    # Set the second dimension to 1 at the indices to add
     x_seq[np.random.randint(length / 10), 1] = 1
     x_seq[np.random.randint(length / 2, length), 1] = 1
-    # Multiply and sum the dimensions of x_seq to get the target value
     y_hat = np.sum(x_seq[:, 0] * x_seq[:, 1])
     return x_seq, y_hat
 
 
-# what can we get from gen_data()
 # print gen_data()
 
 x_seq = T.matrix('input')
